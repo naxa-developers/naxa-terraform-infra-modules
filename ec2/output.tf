5df8cf06f54fdb41ac8874a7e5206e8ae5790219
Nischal Shrestha <nischalstha9@gmail.com>
2024-11-05 08:53:31 +0545

    feat: output ec2 id and ip

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,1 +1,7 @@
+output "ec2_instance_id" {
+  value = aws_instance.ec2_instance.id
+}
 
+output "ec2_instance_ip" {
+  value = var.associate_public_ip_address ? aws_instance.ec2_instance.public_ip : "No Public IP"
+}
